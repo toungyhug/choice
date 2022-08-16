ca5ba5d149e624d2a6f5c75c2c45487b89e115ad
toungyhug <oskarlucewicz@gmail.com>
2022-08-16 11:28:45 +0200

    initial styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="">
-    <p>Hello</p>
+  <div
+    class="w-full h-screen scrollbar-thin scrollbar-track-zinc-400 scrollbar-thumb-stone-200"
+  >
+    <div class="w-full bg-[#fafafa] h-3/4">
+      <p class="">Hello</p>
+    </div>
+    <div class="w-full bg-[#38c26d] h-3/4">
+      <p class="">Hello</p>
+    </div>
   </div>
 </template>
 
